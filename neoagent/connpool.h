2ce355466d7dbc599a7135d71e357ad14624ca90
cubicdaiya <cubicdaiya@gmail.com>
2012-05-11 17:59:49 +0900

    use include guard

--- neoagent/connpool.h
+++ neoagent/connpool.h
@@ -31,6 +31,9 @@
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#ifndef NA_CONNPOOL_H
+#define NA_CONNPOOL_H
+
 #include "env.h"
 
 void na_connpool_create (na_connpool_t *connpool, int c);
@@ -40,3 +43,5 @@ bool na_connpool_assign (na_env_t *env, int *cur, int *fd);
 void na_connpool_init (na_env_t *env);
 na_connpool_t *na_connpool_select(na_env_t *env);
 void na_connpool_switch (na_env_t *env);
+
+#endif // NA_CONNPOOL_H
