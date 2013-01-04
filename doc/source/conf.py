6c6e67d095eeeed67196b20b8382ad8f6a2b7376
cubicdaiya <cubicdaiya@gmail.com>
2013-01-04 17:06:24 +0900

    update copyright
    2012 -> 2013

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'neoagent'
-copyright = u'2012 Tatsuhiko Kubo <cubicdaiya@gmail.com>'
+copyright = u'2013 Tatsuhiko Kubo <cubicdaiya@gmail.com>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
